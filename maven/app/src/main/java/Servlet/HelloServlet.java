46c39cfc766622f9bbd1a2f4f9c0b74547906066
rajeev <rajeev.kumar.phe21@itbhu.ac.in>
2025-04-03 23:46:27 +0530

    new changes

--- maven/app/src/main/java/Servlet/HelloServlet.java
+++ maven/app/src/main/java/Servlet/HelloServlet.java
@@ -1,17 +1,18 @@
 package Servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
+@WebServlet("/hello")  
+public class HelloServlet extends HttpServlet {
 
-import javax.servlet.*;
-
-class HelloServlet extends HttpServlet{
-   @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.getWriter().println("<h1>Hello, Servlet!</h1>");
     }
