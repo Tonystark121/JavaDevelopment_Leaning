05dfafa26d0572d3dea68c15787ff88141692a34
rajeev <rajeev.kumar.phe21@itbhu.ac.in>
2025-04-04 01:28:29 +0530

    post request done by using services

--- maven/app/src/main/java/Servlet/HelloServlet.java
+++ maven/app/src/main/java/Servlet/HelloServlet.java
@@ -12,8 +12,24 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, Servlet!</h1>");
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+       // Corrected: Get "first" and "second" parameters from the request
+       int firstValue = Integer.parseInt(request.getParameter("first"));
+       int secondValue = Integer.parseInt(request.getParameter("second"));
+
+       System.out.println("The inputted values: " + firstValue + " " + secondValue);
+
+       // Calculate the sum of the values
+       int totalSum = firstValue + secondValue;
+
+       System.out.println("Total of Input: " + totalSum);
+
+       // Setting the content type for the response
+       response.setContentType("text/html");
+
+       // Writing the output to the response
+       response.getWriter().println("<h1>Hello, Servlet!</h1>");
+       response.getWriter().println("Sum of the values is: " + totalSum);
     }
 }
